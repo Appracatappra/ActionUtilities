17e2a6c3cf0103e388f54159e84da65fd3e99f29
Kevin Mullins <kevin_mullins@appracatappra.com>
2017-11-30 11:52:39 -0600

    Initial Setup
    Setting up project to move elements from Action Controls.

---
+++ ActionUtilities/ActionUtilities.h
@@ -0,0 +1,19 @@
+//
+//  ActionUtilities.h
+//  ActionUtilities
+//
+//  Created by Kevin Mullins on 11/30/17.
+//  Copyright © 2017 Appracatappra, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActionUtilities.
+FOUNDATION_EXPORT double ActionUtilitiesVersionNumber;
+
+//! Project version string for ActionUtilities.
+FOUNDATION_EXPORT const unsigned char ActionUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionUtilities/PublicHeader.h>
+
+
