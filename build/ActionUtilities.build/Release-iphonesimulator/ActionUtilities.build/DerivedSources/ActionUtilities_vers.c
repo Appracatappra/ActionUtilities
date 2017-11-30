14a70b504c6a5f574ae4ef339e758f74379a2045
Kevin Mullins <kevin_mullins@appracatappra.com>
2017-11-30 13:14:57 -0600

    Properly add .gitignore to repo

--- build/ActionUtilities.build/Release-iphonesimulator/ActionUtilities.build/DerivedSources/ActionUtilities_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ActionUtilitiesVersionString[];
- extern const double ActionUtilitiesVersionNumber;
-
- const unsigned char ActionUtilitiesVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ActionUtilities  PROJECT:ActionUtilities-1" "\n";
- const double ActionUtilitiesVersionNumber __attribute__ ((used)) = (double)1.;
--- build/ActionUtilities.build/Release-iphonesimulator/ActionUtilities.build/Objects-normal/x86_64/ActionUtilities_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kmullins/Xcode\ Projects/GitHub/ActionUtilities/build/ActionUtilities.build/Release-iphonesimulator/ActionUtilities.build/DerivedSources/ActionUtilities_vers.c
